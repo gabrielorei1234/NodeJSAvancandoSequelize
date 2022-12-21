3a3aaebc9deb788470f3bff7c11604be75dabe55
Gabriel Barros de Oliveira <gabriel.oliveira@INTRANET.LOCAL>
2022-12-21 18:39:10 -0300

    adição filtro de  data, sql puro

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -9,6 +9,8 @@ router
   .get('/pessoas/:id', PessoaController.pegaUmaPessoa)
   .get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.pegaUmaMatricula)
   .get('/pessoas/:estudanteId/matricula', PessoaController.pegaMatriculas)
+  .get('/pessoas/matricula/:turmaId/confirmadas', PessoaController.pegaMatriculasPorTurma)
+  .get('/pessoas/matricula/lotada', PessoaController.pegaTurmasLotadas)
   .post('/pessoas', PessoaController.criaPessoa)
   .put('/pessoas/:id', PessoaController.atualizaPessoa)
   .delete('/pessoas/:id', PessoaController.apagaPessoa)
